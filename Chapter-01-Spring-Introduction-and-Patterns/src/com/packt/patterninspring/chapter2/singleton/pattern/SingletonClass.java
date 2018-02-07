cbc0c9660a648b8a6dc0d5e6b9dcebd5b6bd23ce
XinyiLiu <Xinyi.liu@quarkfinance.com>
2018-02-07 17:01:39 +0800

    git ignore

--- Chapter-01-Spring-Introduction-and-Patterns/src/com/packt/patterninspring/chapter2/singleton/pattern/SingletonClass.java
+++ Chapter-01-Spring-Introduction-and-Patterns/src/com/packt/patterninspring/chapter2/singleton/pattern/SingletonClass.java
@@ -5,16 +5,18 @@ package com.packt.patterninspring.chapter2.singleton.pattern;
  *
  */
 public class SingletonClass {
-	private static SingletonClass instance = null;
+    private static volatile SingletonClass instance = null;
+
     private SingletonClass() {
     }
+
     public static SingletonClass getInstance() {
         if (instance == null) {
-        	synchronized(SingletonClass.class){  
-        		if (instance == null) {
-        			instance = new SingletonClass();
-        		}
-        	}
+            synchronized (SingletonClass.class) {
+                if (instance == null) {
+                    instance = new SingletonClass();
+                }
+            }
         }
         return instance;
     }
